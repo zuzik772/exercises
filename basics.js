8f44ebc44b415e285371b498dd78820f430319ae
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-02-04 13:10:24 +0100

    add js

--- basics.js
+++ basics.js
@@ -1,1 +1,34 @@
-console.log("have a nice day");
+"use strict";
+// let y = 14;
+// console.log(y);
+// console.log("exercise");
+// const bool = true;
+// const num = 41;
+// const str = "Peter";
+// const obj = {
+//   cats: 2,
+//   cars: 1,
+// };
+// const nothing = null;
+// let undf;
+// const symbol = Symbol("symbol");
+
+// console.log(bool);
+// console.log(num);
+// console.log(str);
+// console.log(obj);
+// console.log(nothing);
+// console.log(undf);git add 
+// console.log(symbol);
+
+// const n1 = 1;
+// const n2 = 2;
+// const s1 = "1";
+// const s2 = "2";
+
+// const value = "";
+// if (value) {
+//   console.log("Value is truthy");
+// } else {
+//   console.log("Value is falsy");
+// }
