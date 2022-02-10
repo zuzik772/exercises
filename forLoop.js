446f8fd5f948c65a3392a7c412ce33459748178c
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-02-10 11:26:29 +0100

    for loop exercise

---
+++ forLoop.js
@@ -0,0 +1,15 @@
+// for (let counter = 10; counter >= 0; counter--) {
+//   console.log(counter);
+// }
+// console.log("liftoff");
+
+// only counts odd numbers
+// counter += 2;
+// counter = counter + 2;
+// for (let counter = 1; counter <= 19; counter =counter+2) {
+//   console.log(counter);
+// }
+
+for (let counter = 1; counter <= 19; counter += 2) {
+  console.log(counter);
+}
