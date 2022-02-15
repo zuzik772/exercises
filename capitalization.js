198863b51a60262dac5c4dd861a8d949c5c91eb2
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-02-15 21:04:04 +0100

    fullname return

--- capitalization.js
+++ capitalization.js
@@ -5,7 +5,11 @@
 
 function capitalize(str) {
   str = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-  console.log(str);
+  return str;
 }
 
-capitalize("agA sSu");
+console.log(capitalize("agAtka"));
+
+const name = "pETer";
+const cap = capitalize(name);
+console.log(cap);
--- nameparts.js
+++ nameparts.js
@@ -21,7 +21,8 @@ console.log("LN", lastName);
 
 //  using function and parameters
 function giveName(firstName, middleName, lastName) {
-  console.log({ firstName, middleName, lastName });
+  // console.log({ firstName, middleName, lastName });
+  return firstName + middleName + lastName;
 }
 
 giveName(firstName, middleName, lastName);
@@ -29,12 +30,16 @@ giveName(firstName, middleName, lastName);
 // fullname exercise
 function fullName(lastName, firstName, middleName) {
   if (middleName !== undefined) {
-    console.log(`${firstName} ${middleName} ${lastName}`);
+    // console.log(`${firstName} ${middleName} ${lastName}`);
+    return firstName + middleName + lastName;
   } else {
-    console.log(`${firstName} ${lastName}`);
+    // console.log(`${firstName} ${lastName}`);
+    return firstName + lastName;
   }
 }
 
-fullName("Potter", "Harry", "James");
-fullName("Potter", "Harry");
+let result = fullName("Potter", "Harry", "James");
+result = fullName("Potter", "Harry");
 fullName("Potter", "Harry", "James", "Pottypotpot");
+
+console.log(result);
